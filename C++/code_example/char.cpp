73eedaaf3f7319eff1702e49bb8cdc0ee2db4c60
Bound-whd <boundwhd@126.com>
2025-01-16 17:16:35 +0800

    2025年1月16日17:16:33

--- C++/code_example/char.cpp
+++ C++/code_example/char.cpp
@@ -3,33 +3,64 @@
 #include <string.h>
 using namespace std;
 
+
+// test1 字符串存储在栈中
+void test1(){
+    char str[] = "abc";                
+    //1. 栈中的字符串可以修改
+    str[0] = 'w';
+    cout << str << endl;
+
+    char str1[] = "abcd";  
+    char str2[] = "abce";  
+    //2. 地址相同与字符串匹配
+    cout << "地址：" << (str1 == str2) <<endl;
+    // 字符串相同需要便利整个字符串，变量存放的是第一个字符的地址
+    cout << "字符串数组第一个字符：" << (*str1 == *str2) <<endl;
+}
+
+// 字符串存储在常量区中
+void test2(){
+
+    //字符串常量在 C++ 中被定义为只读内存，而将它赋值给一个 char*，字符串无法修改
+    const char* str = "abc";
+    
+    //输出字符串
+    cout << str << endl;
+
+    //输出指针地址
+    cout << (void *)str << endl;
+
+    //输出字符串的第一个字符（a）
+    cout << *str << endl;
+
+
+    // 字符串相等判断
+    const char* str1 = "abb";
+    const char* str2 = "abc";
+
+    cout << (str1 == str2) << endl;
+    cout << (*str1 == *str2) << endl;
+    /*对于相同的字符串常量，系统会把他们优化，指向同一个内存区域*/
+}
+
+//
+void test3(){
+    //字符串存放在堆区
+    char* str = (char*)malloc(5);
+    const char* str1 = "abcd";
+
+    //把str1的字符串复制到str中
+    strcpy(str, str1);
+
+    //比较地址是否相同
+    cout << (str == str1) << endl;
+
+}
+
+
 int main(){
 
-    char str1[] = "abc";                //字符串存储在栈中
-    char str2[] = "abc";
-
-    bool a = (*str1 == *str2);
-    cout  << a << endl;
-    str1[0] = 'b';
-    cout << str1 << endl;
-
-    // 字符串常量在 C++ 中被定义为 只读内存，而将它赋值给一个 char* 
-    // 意味着你试图修改这块只读内存中的内容，这在现代 C++ 中是被禁止的。
-
-    const char *str3 = "abc";         //字符串存储在常量区
-    const char *str4 = "abc";
-    printf("%p\n", (void *)str3);
-
-    char *str5 = (char*)malloc(4);      //字符串存储在堆区
-    strcpy(str5,str1);      
-    a = (str5 == str1);
-    cout << a << endl;
-
-    char c = 'a';
-    char b = 'a';
-
-    a = (c == b);
-    cout << a << endl;
-
+    test3();
     return 0;
 }
--- C++/code_example/static.cpp
+++ C++/code_example/static.cpp
@@ -1,45 +1,45 @@
-#include <stdio.h>
 #include <iostream>
-
 using namespace std;
 
-class Stu
-{
-public:
-	static int age;
-	
-    static int add(int a, int b)
-    {
-        return a + b;
-    }
-	int BMI = weight + height;
+class MyClass {
 private:
-	static int weight;
-	static int height;
+    // 静态成员变量
+    static int staticVar;
+
+public:
+    // 构造函数
+    MyClass() {
+        // 每次创建对象时打印一个消息
+        cout << "Object created!" << endl;
+    }
+
+    // 静态成员函数
+    static void incrementStaticVar() {
+        staticVar++;  // 增加静态成员变量的值
+    }
+
+    // 静态成员函数，返回静态变量
+    static int getStaticVar() {
+        return staticVar;
+    }
 };
 
-int Stu::age = 19;
-int Stu::weight = 119;
-int Stu::height = 180;
+// 定义静态成员变量
+int MyClass::staticVar = 0;
 
-int main()
-{   
+int main() {
+    // 通过类名直接访问静态成员函数
+    MyClass::incrementStaticVar();  // 静态函数通过类名访问
+    MyClass::incrementStaticVar();
+    MyClass::incrementStaticVar();
 
-	static int whd;
-	cout << whd << endl;
-    int c = Stu::add(1, 2);
-    cout << c << endl;
-	cout<<Stu::age<<endl;//输出19；
-	// cout<<Stu::height<<endl;//错误的，私有无法访问。
-	Stu s;
-	cout<<s.age<<endl;//输出19；
-	cout << s.BMI << endl;
-	// cout<<s.height<<endl;//错误的，私有无法访问。
-	return 0;
+    // 通过类名访问静态成员变量
+    cout << "Static Variable Value: " << MyClass::getStaticVar() << endl;
+
+    // 创建对象，通过对象访问静态成员
+    MyClass obj1;
+    obj1.incrementStaticVar();
+    cout << "Static Variable Value (after object increment): " << MyClass::getStaticVar() << endl;
+
+    return 0;
 }
-
-/*静态成员可以不用实例化对象调用，并且所有对象共享这个静态变量成员*/
-/*静态成员必须类外初始化*/
-/*静态成员初始化可以不赋初值*/
-
-
