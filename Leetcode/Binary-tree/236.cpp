861b2911ec25fcea7cb82fcc7c95cdd3e592f476
王海东 <boundwhd@126.com>
2025-02-13 00:10:45 +0800

    2025年2月13日00:10:43

---
+++ Leetcode/Binary-tree/236.cpp
@@ -0,0 +1,9 @@
+/*二叉树的最近公共祖先*/
+#include "../leetcode.h"
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    stack<TreeNode*> stk_p;
+    stack<TreeNode*> stk_q;
+
+    
+}
