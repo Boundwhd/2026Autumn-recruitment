3ac471e9b1c4ed5c05eb1f8502721d06c8b35184
Bound-whd <boundwhd@126.com>
2025-03-19 16:02:22 +0800

    ignore

--- HPC/cuda/gemm/build/CMakeFiles/clean_times.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/clean_times"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/clean_times.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
